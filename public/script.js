e2ac7f75f6070b7f28ff8533ea77a5448b18b8af
SowmyaSeshadri <seshadrisowmya5@gmail.com>
2018-02-05 10:48:35 +0530

    Comments added

--- public/script.js
+++ public/script.js
@@ -2,43 +2,41 @@ var ul = document.querySelector("ul");
 var todosUl = window.document.querySelector('ul');
 var fragment = window.document.createDocumentFragment();
 
-window.onload=function() {
+window.onload = function () {
     init();
 }
 
-function xmlrequest(type,url,content=null,func=null) {
+function xmlrequest(type, url, content = null, func = null) {
     // define the type of request either get,put,delete or post
-        var request = new window.XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if(request.readyState == 4&&request.status == 200) {
-                if(func!=null) {
-                    console.log(request.responseText);
+    var request = new window.XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            if (func != null) {
+                console.log(request.responseText);
                 func(request.responseText);
-                }      
             }
-        };
-        request.open(type,url,true);
-        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        request.send(content);
+        }
+    };
+    request.open(type, url, true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(content);
 }
-
-
 
 function init() {
     $('#add-item-button').click(addList);
     // $('#get-item-button').click(getList);
-    $('#to-do-list-items').click(function updateAndDelete1(event){
+    $('#to-do-list-items').click(function updateAndDelete1(event) {
         updateAndDelete(event);
     });
-        
+
 }
 
 function addList() {
     var text = $('#add-list-item').val().replace(/^\s+$/g, '');
-    if(text.length == 0) {
+    if (text.length == 0) {
         alert('Enter the task in the text field');
     } else {
-        xmlrequest("post","listItem","desc="+text,addTodosToPage);
+        xmlrequest("post", "listItem", "desc=" + text, addTodosToPage);
     }
 }
 
@@ -51,7 +49,7 @@ var view = {
         delButton.classList.add('delete-button');
         delButton.classList.add('list-button');
         delButton.className = 'delete-button list-button';
-        delButton.dataset.id="delete-item-button-"+id;
+        delButton.dataset.id = "delete-item-button-" + id;
         return delButton;
     },
     createStrikeButton: function (id) {
@@ -59,24 +57,25 @@ var view = {
         strikeButton.textContent = 'Completed';
         strikeButton.classList.add('checked-button');
         strikeButton.classList.add('list-button');
-        strikeButton.dataset.id="update-item-button-"+id;
+        strikeButton.dataset.id = "update-item-button-" + id;
         return strikeButton;
     },
     createUIItem: function (todoItem) {
-    var id = todoItem.id;
-    var newList = document.createElement('li');
-    newList.classList.add('list');
-    var listText = document.createElement('div');
-    listText.classList.add('list-text'); 
-    listText.textContent=todoItem.desc;
-    listText.dataset.id='list-text-'+id;
-    newList.dataset.id='list-item-'+id;
-    newList.appendChild(listText);
-    newList.appendChild(view.createStrikeButton(id));
-    newList.appendChild(view.createDelButton(id));
-    return newList;       
+        var id = todoItem.id;
+        var newList = document.createElement('li');
+        newList.classList.add('list');
+        var listText = document.createElement('div');
+        listText.classList.add('list-text');
+        listText.textContent = todoItem.desc;
+        listText.dataset.id = 'list-text-' + id;
+        newList.dataset.id = 'list-item-' + id;
+        newList.appendChild(listText);
+        newList.appendChild(view.createStrikeButton(id));
+        newList.appendChild(view.createDelButton(id));
+        return newList;
     }
 };
+
 //Adding todos to page after retrieving
 function addTodosToPage(todos) {
     var toDo = JSON.parse(todos);
@@ -86,12 +85,12 @@ function addTodosToPage(todos) {
         var todoItem = toDo.listItems[i];
         fragment.appendChild(view.createUIItem(todoItem));
     }
-    element.insertBefore(fragment,element.childNodes[0]); 
+    element.insertBefore(fragment, element.childNodes[0]);
 }
 //Retreive todoItems 
 window.document.getElementById('get-item-button').addEventListener('click', function () {
 
-    xmlrequest('GET','listItem',null,addTodosToPage);
+    xmlrequest('GET', 'listItem', null, addTodosToPage);
     document.getElementById('get-item-button').classList.add("dont-display");
 });
 
@@ -99,20 +98,20 @@ window.document.getElementById('get-item-button').addEventListener('click', func
 function updateAndDelete(event) {
     var element = event.target;
     var getId = element.dataset.id.split('-');
-    if(getId[0] == 'delete') {
-        if(window.confirm('Do you want to delete the selected list item?') == true) {
-            window.document.querySelector('li[data-id="list-item-'+getId[3]+'"]').remove();
-            xmlrequest("delete", "listItem?id="+getId[3], null, null);
+    if (getId[0] == 'delete') {
+        if (window.confirm('Do you want to delete the selected list item?') == true) {
+            window.document.querySelector('li[data-id="list-item-' + getId[3] + '"]').remove();
+            xmlrequest("delete", "listItem?id=" + getId[3], null, null);
         }
-    } else if(getId[0] == 'update') {
-        if(document.querySelector('div[data-id="list-text-' + getId[3]+'"]').classList.contains('line-through')){
-            document.querySelector('div[data-id="list-text-' + getId[3]+'"]').classList.remove('line-through');
+    } else if (getId[0] == 'update') {
+        if (document.querySelector('div[data-id="list-text-' + getId[3] + '"]').classList.contains('line-through')) {
+            document.querySelector('div[data-id="list-text-' + getId[3] + '"]').classList.remove('line-through');
             element.innerHTML = 'Check';
-            xmlrequest("put", "checkListItem?id="+getId[3], null, null);
+            xmlrequest("put", "checkListItem?id=" + getId[3], null, null);
         } else {
-            document.querySelector('div[data-id="list-text-' + getId[3]+'"]').classList.add('line-through');
+            document.querySelector('div[data-id="list-text-' + getId[3] + '"]').classList.add('line-through');
             element.innerHTML = 'Uncheck';
-            xmlrequest("put", "uncheckListItem?id="+getId[3], null, null);
+            xmlrequest("put", "uncheckListItem?id=" + getId[3], null, null);
         }
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,83 +1,75 @@
 var express = require('express');
 var router = express.Router();
 
-var toDoList= {'listItems':[],'noOfItemsChecked':0};
+//TODO Array 
+var toDoList = { 'listItems': [], 'noOfItemsChecked': 0 };
 
+//Function to generate ID
 function getId() {
-  if(toDoList.listItems.length==0) {
+  if (toDoList.listItems.length == 0) {
     return 1;
   } else {
-    return (toDoList.listItems[(toDoList.listItems.length)-1].id)+1;
+    return (toDoList.listItems[(toDoList.listItems.length) - 1].id) + 1;
   }
 }
 
-
-function createListItem(id,desc) {
-  this.id=id;
-  this.desc=desc;
-  this.ischecked=false;
+//Constructor to add todo Items 
+function createListItem(id, desc) {
+  this.id = id;
+  this.desc = desc;
+  this.ischecked = false;
 }
 
-/* GET home pag
-
-
-router.post('/listItem',function(req,res,next) {
-  //Naveen's code
-});e. */
-router.get('/goodbye', function(req, res, next) {
-  res.end("bye");
-  // res.render('index', { title: 'Express' });
+//POST request - To add todos
+router.post('/listItem', function (req, res, next) {
+  var id = getId();
+  var duplicateToDoList = { 'listItems': [], 'noOfItemsChecked': 0 };
+  var listItem = new createListItem(id, req.body.desc);
+  toDoList.listItems.push(listItem);
+  duplicateToDoList.listItems.push(listItem);
+  res.end(JSON.stringify(duplicateToDoList));
 });
 
-
-router.post('/listItem',function(req,res,next) {
-    var id = getId();
-    var duplicateToDoList= {'listItems':[],'noOfItemsChecked':0};
-    var listItem = new createListItem(id,req.body.desc);
-    toDoList.listItems.push(listItem);
-    duplicateToDoList.listItems.push(listItem);
-    res.end(JSON.stringify(duplicateToDoList));
-});
-
-
-router.get('/listItem',function(req,res,next) {
-  if(toDoList == null)
-  {
+//GET request - To retrieve todos
+router.get('/listItem', function (req, res, next) {
+  if (toDoList == null) {
     res.send("Nothing to Display");
   }
   else {
-   res.send(toDoList);
+    res.send(toDoList);
   }
 });
 
-router.put('/checkListItem',function(req,res,next) {
+//PUT request - To Update Todos
+router.put('/checkListItem', function (req, res, next) {
   var id = req.query.id;
-  var index = toDoList.listItems.findIndex(function(item, i) {
+  var index = toDoList.listItems.findIndex(function (item, i) {
     return item.id == id;
   });
   toDoList.noOfItemsChecked--;
   toDoList.listItems[index].ischecked = false;
 });
 
-router.put('/uncheckListItem',function(req,res,next) {
+router.put('/uncheckListItem', function (req, res, next) {
   var id = req.query.id;
-  var index = toDoList.listItems.findIndex(function(item) {
+  var index = toDoList.listItems.findIndex(function (item) {
     return item.id == id;
   });
   toDoList.noOfItemsChecked++;
   toDoList.listItems[index].ischecked = true;
 });
 
-router.delete('/listItem',function(req,res,next) {
+//DELETE request - To delete Todos
+router.delete('/listItem', function (req, res, next) {
   var id = req.query.id;
-  var list_array=toDoList.listItems;
-  var index = list_array.findIndex(function(element) {
-    return element.id==id;
+  var list_array = toDoList.listItems;
+  var index = list_array.findIndex(function (element) {
+    return element.id == id;
   });
-  if( list_array[index].ischecked===true)
-  {
+  if (list_array[index].ischecked === true) {
     toDoList.noOfItemsChecked--;
   }
-  toDoList.listItems.splice(index,1);
+  toDoList.listItems.splice(index, 1);
 });
+
 module.exports = router;
