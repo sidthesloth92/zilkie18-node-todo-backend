af9f16fc989a2e27c5005582a744e42da384d4ec
SowmyaSeshadri <seshadrisowmya5@gmail.com>
2018-02-06 06:40:11 +0530

    GET DONE after executeQuery

--- routes/index.js
+++ routes/index.js
@@ -18,13 +18,13 @@ function executeQuery(statement, queryParameters, callback, request, response) {
   var con = dbconfig.getConnection();
   con.connect(function (err) {
     if (err) throw err;
-      con.query(statement, queryParameters, function (err, result) {
-        if (err) throw err;
-        if (callback != undefined) {
-          callback(result, request, response);
-           con.end();
-        }
-      });    
+    con.query(statement, queryParameters, function (err, result) {
+      if (err) throw err;
+      if (callback != undefined) {
+        callback(result, request, response);
+        con.end();
+      }
+    });
   });
 }
 //POST request - To add todos
@@ -45,92 +45,105 @@ function postResponse(result, req, res) {
   res.end(JSON.stringify(listItem));
 }
 
+//GET request - Retrieve data
+router.get('/list-item', function (req, res, next) {
+  var selectStatement = queries.GETQUERY;
+  executeQuery(selectStatement, null, getTodo, req, res);
+});
+
+function getTodo(result, req, res) {
+  var resultData = result;
+  if (resultData == null) {
+    res.send("Nothing to Display");
+  }
+  else {
+    console.log(JSON.stringify(resultData));
+    res.send(resultData);
+  }
+};
 
 
 
+  // //GET request - To retrieve todos
+  // router.get('/list-item', function (req, res, next) {
+  //   var resultData;
+  //   var con = createConnection();
+  //   con.connect(function (err) {
+  //     if (err) throw err;
+  //     var sql = "select * from todo_data"
+  //     con.query(sql, function (err, result) {
+  //       if (err) throw err;
+  //       else {
+  //         resultData = result;
+  //         if (resultData == null) {
+  //           res.send("Nothing to Display");
+  //         }
+  //         else {
+  //           console.log(JSON.stringify(resultData));
+  //           res.send(resultData);
+  //         }
+  //       }
+  //       con.end();
+  //     });
+  //   });
+  // });
+
+  // //PUT request - To Update Todos
+  // router.put('/list-item', function (req, res, next) {
+  //   var id = req.body.id;
+  //   var con = createConnection();
+  //   var sql = "SELECT is_checked from todo_data where id = " + id;
+  //   con.connect(function (err) {
+  //     if (err) throw err;
+  //   });
+  //   con.query(sql, function (err, result) {
+  //     if (err) throw err;
+  //     con.end();
+  //     con = createConnection();
+  //     if (result[0].is_checked == 0) {
+  //       sql = "UPDATE todo_data SET is_checked = 1 WHERE id = " + id;
+  //     } else {
+  //       sql = "UPDATE todo_data SET is_checked = 0 WHERE id = " + id;
+  //     }
+  //     con.connect(function (err) {
+  //       if (err) { 
+  //         throw err;
+  //       }
 
 
+  //       con.query(sql, function (err, result) {
+  //         if (err) {
+  //            throw err;
+  //         }
+  //            con.end();
+  //       });
+  //     });
+  //   });
+  //   if (toDoList.listItems[index].isChecked == false) {
+  //     toDoList.listItems[index].isChecked = true;
+  //   } else {
+  //     toDoList.listItems[index].isChecked = false;
+  //   }
+  //   console.log(toDoList);
+  // });
 
-// //GET request - To retrieve todos
-// router.get('/list-item', function (req, res, next) {
-//   var resultData;
-//   var con = createConnection();
-//   con.connect(function (err) {
-//     if (err) throw err;
-//     var sql = "select * from todo_data"
-//     con.query(sql, function (err, result) {
-//       if (err) throw err;
-//       else {
-//         resultData = result;
-//         if (resultData == null) {
-//           res.send("Nothing to Display");
-//         }
-//         else {
-//           console.log(JSON.stringify(resultData));
-//           res.send(resultData);
-//         }
-//       }
-//       con.end();
-//     });
-//   });
-// });
+  // //DELETE request - To delete Todos
+  // router.delete('/list-item/:id', function (req, res, next) {
+  //   var id = req.params.id;
+  //   var con = createConnection();
+  //   con.connect(function (err) {
+  //     if (err) {
+  //       throw new Error("Connection Failed");
+  //     }
+  //     var delete_query = "delete from todo_data where id=" + id;
+  //     con.query(delete_query, function (err, result) {
+  //       if (err) {
+  //         throw new Error("Query Failed");
+  //       }
+  //       con.end();
+  //     });
+  //   });
 
-// //PUT request - To Update Todos
-// router.put('/list-item', function (req, res, next) {
-//   var id = req.body.id;
-//   var con = createConnection();
-//   var sql = "SELECT is_checked from todo_data where id = " + id;
-//   con.connect(function (err) {
-//     if (err) throw err;
-//   });
-//   con.query(sql, function (err, result) {
-//     if (err) throw err;
-//     con.end();
-//     con = createConnection();
-//     if (result[0].is_checked == 0) {
-//       sql = "UPDATE todo_data SET is_checked = 1 WHERE id = " + id;
-//     } else {
-//       sql = "UPDATE todo_data SET is_checked = 0 WHERE id = " + id;
-//     }
-//     con.connect(function (err) {
-//       if (err) { 
-//         throw err;
-//       }
+  // });
 
-
-//       con.query(sql, function (err, result) {
-//         if (err) {
-//            throw err;
-//         }
-//            con.end();
-//       });
-//     });
-//   });
-//   if (toDoList.listItems[index].isChecked == false) {
-//     toDoList.listItems[index].isChecked = true;
-//   } else {
-//     toDoList.listItems[index].isChecked = false;
-//   }
-//   console.log(toDoList);
-// });
-
-// //DELETE request - To delete Todos
-// router.delete('/list-item/:id', function (req, res, next) {
-//   var id = req.params.id;
-//   var con = createConnection();
-//   con.connect(function (err) {
-//     if (err) {
-//       throw new Error("Connection Failed");
-//     }
-//     var delete_query = "delete from todo_data where id=" + id;
-//     con.query(delete_query, function (err, result) {
-//       if (err) {
-//         throw new Error("Query Failed");
-//       }
-//       con.end();
-//     });
-//   });
-
-// });
-
-module.exports = router;
+  module.exports = router;
